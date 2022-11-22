1b4791abbbf26ec44aaddadd0d24fa89a6d1a5df
Chad Kostohryz <kostc7@gmail.com>
2022-11-22 09:35:41 -0600

    Added choices to the list.

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ let wpyAll
 
 async function init() {
     let newConnection = await connection
-    wypAll = new wyp(newConnection)
+    wpyAll = new wyp(newConnection)
     loadQ()
 }
 
@@ -28,12 +28,68 @@ async function loadQ() {
     ]).then(async (data) => {
         switch (data.action) {
             case 'View all employees':
-                await wypAll.viewEmploy()
+                await wpyAll.viewEmploy()
                 loadQ()
                 break
             case 'Add employee':
                 addEmployee()
                 break
+            case 'Update employee role':
+                updateEmployeeRole
+                break
+            case 'View all roles':
+                await wpyAll.viewRoles()
+                loadQ()
+                break
+            case 'Add role':
+                addRole()
+                break
+            case 'View all departments':
+                await wpyAll.viewDepartment()
+                loadQ()
+                break
+            case 'All departments':
+                addDepartmnet()
+                break
+            case 'Exit':
+                let myConnection = await connection()
+                myConnection.end()
         }
     })
 }
+
+async function addEmployee(){
+    const roleChoices = await wpyAll.getRoles()
+    const employeeChoices = await wpyAll.getEmployees()
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'firstname',
+            message: 'The first name of employee?'
+        },
+        {
+            type: 'input',
+            name: 'lastname',
+            message: 'The last name of employee?'
+        },
+        {
+            type: 'input',
+            name: 'role_id',
+            message: 'What is the role id?',
+            choices: roleChoices
+        },
+        {
+            type: 'input',
+            name: 'manager_id',
+            message: 'What is the manager id?',
+            choices: employeeChoices
+        },
+    ]).then(async (data) => {
+        await wpyAll.addEmployee(data.firstname, data.lastname, data.role_id, data.manager_id)
+        console.log('New employee created')
+
+        loadQ()
+    })
+}
+
